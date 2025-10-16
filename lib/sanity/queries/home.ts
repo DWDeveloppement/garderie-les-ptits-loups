e6adc9d5d335a9e97619ea4e0e9e5a6bab20d257
Ricardo Do Vale <pataco80@gmail.com>
2025-10-16 20:27:52 +0200

    feat: add home page query with populated relations
    - Create HOME_QUERY with single-fetch strategy
    - Populate linkedSectors with -> operator (avoid N+1)
    - Populate linkedOtherSpaces in same query
    - Include all necessary data (hero, parallax, SEO)
    - Add fetchHome() with cache tag 'home-page'
    - Query optimized for SSG performance

---
+++ lib/sanity/queries/home.ts
@@ -0,0 +1,59 @@
+import { groq } from 'next-sanity'
+import { sanityFetch } from '../client'
+import { BASIC_IMAGE_QUERY } from '../helpers/imageProps'
+
+/**
+ * Query pour la page d'accueil
+ * Populate tous les secteurs et espaces liés en 1 requête
+ */
+export const HOME_QUERY = groq`
+  *[_type == "home" && _id == "home"][0] {
+    title,
+    
+    // Hero
+    sectionHero {
+      image ${BASIC_IMAGE_QUERY},
+      description
+    },
+    
+    // Secteurs liés (populate avec ->)
+    "linkedSectors": linkedSectors[]-> {
+      _id,
+      title,
+      "slug": devConfig.slug.current,
+      sectionHero {
+        image ${BASIC_IMAGE_QUERY},
+        description
+      }
+    },
+    
+    // Autres espaces liés
+    "linkedOtherSpaces": linkedOtherSpaces[]-> {
+      _id,
+      title,
+      sector,
+      image ${BASIC_IMAGE_QUERY},
+      description
+    },
+    
+    // Contenu page
+    contentPage,
+    
+    // Parallax
+    parallax {
+      image ${BASIC_IMAGE_QUERY}
+    },
+    
+    // SEO
+    seo {
+      metaTitle,
+      metaDescription,
+      keywords,
+      shareImage ${BASIC_IMAGE_QUERY}
+    }
+  }
+`
+
+export async function fetchHome() {
+	return sanityFetch(HOME_QUERY, {}, { tag: 'home-page' })
+}
